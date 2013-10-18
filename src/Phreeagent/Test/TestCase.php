ff067c9471293e025010c291d86e2cceefc313b5
Nick Jones <nick@nicksays.co.uk>
2013-10-18 20:27:52 +0100

    Add a response stub method to our base test case

--- src/Phreeagent/Test/TestCase.php
+++ src/Phreeagent/Test/TestCase.php
@@ -28,4 +28,38 @@ class TestCase extends \PHPUnit_Framework_TestCase
 
         return $configuration;
     }
+
+
+    /**
+     * @param $filename
+     * @param $status_code
+     * @param array $headers
+     * @return \Requests_Response
+     * @throws \Exception
+     */
+    protected function loadMockResponse($filename, $status_code, $headers = array())
+    {
+        $full_filename = sprintf("%s/raw_response/%s", __DIR__, $filename);
+
+        if (file_exists($full_filename)) {
+            if (is_readable($full_filename)) {
+                $file_contents = file_get_contents($full_filename);
+
+                $response = new \Requests_Response();
+                $response->body = $file_contents;
+                $response->status_code = $status_code;
+                $response->success = ($status_code >= 200 && $status_code < 300);
+
+                foreach ($headers as $key => $value) {
+                    $response->headers[$key] = $value;
+                }
+
+                return $response;
+            } else {
+                throw new \Exception("Fixture $full_filename is not readable");
+            }
+        } else {
+            throw new \Exception("Fixture $full_filename does not exist");
+        }
+    }
 }
